8058f632b212459fb34a3863fb8b206106ef766f
Mike Ward <ward.michael51@gmail.com>
2016-02-20 14:08:58 -0500

    theme wrapped into partials

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 (function(){
     'use strict';
     
-    angular.module('myApp', []);
+    angular.module('mward', []);
     
     angular
-        .module('myApp')
+        .module('mward')
         .controller('mainCtrl', ['$scope', function($scope){
             $scope.title = "Main Page";
         }]);
 
-}())
+    angular
+        .module('mward')
+        .directive('mwardDevMap', [function () {
+            return {
+                templateUrl: '/public/templates/map-directive.html',
+                replace: true,
+                link: function () {
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                        center: {
+                            lat: 38.2,
+                            lng: -85.76
+                        },
+                        scrollwheel: false,
+                        zoom: 10
+                    });
+                }
+            };
+        }]);
+})();
+
